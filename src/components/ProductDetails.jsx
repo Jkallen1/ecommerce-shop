db9aca24dc62ace0f24455398936c9781b695f1b
Joshua Allen <Jayallen11@outlook.com>
2023-06-18 17:20:03 -0400

    updated fetch products function

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -1,39 +1,49 @@
-import React, {useState, useEffect} from 'react';
-import {useParams} from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
 function ProductDetails() {
-    const { productId } = useParams();
-    const [product, setProduct] = useState(null);
+  const { id } = useParams();
+  const [product, setProduct] = useState(null);
 
-    useEffect(() => {
-        fetchProductDetails(productId)
-        .then((product) => setProduct(product))
-        .catch((error) => console.error('Error fetching product details:', error))
-    }, [productId])
+  useEffect(() => {
+    fetchProductDetails(id)
+      .then((product) => setProduct(product))
+      .catch((error) => console.error('Error fetching product details:', error));
+  }, [id]);
 
-    async function fetchProductDetails(productId) {
-        try {
-            const response = await fetch(`https://fakestoreapi.com/products/${productId}`);
-            const product = await response.json();
-            return product;
-        } catch(error) {
-            throw error;
-        }
+  async function fetchProductDetails(productId) {
+    try {
+      const response = await fetch(`https://fakestoreapi.com/products/${productId}`);
+
+      if (!response.ok) {
+        throw new Error('Error fetching product details');
+      }
+
+      const productData = await response.json();
+
+      if (Object.keys(productData).length === 0) {
+        throw new Error('Empty response');
+      }
+
+      return productData;
+    } catch (error) {
+      throw error;
     }
+  }
 
-    if (!product) {
-        return <p>Loading Product Details...</p>;
-    }
+  if (!product) {
+    return <p>Loading Product Details...</p>;
+  }
 
-    return (
-        <div>
-            <h1>Product Details</h1>
-            <p>Product ID: {productId}</p>
-            <img src= {product.image} alt= {product.title} />
-            <h3>{product.title}</h3>
-            <p>{product.description}</p>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Product Details</h1>
+      <p>Product ID: {id}</p>
+      <img src={product.image} alt={product.title} />
+      <h3>{product.title}</h3>
+      <p>{product.description}</p>
+    </div>
+  );
 }
 
 export default ProductDetails;
